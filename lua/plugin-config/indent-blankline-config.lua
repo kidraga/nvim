752f86489c3090a7a516ac700e04d8d206fd5674
Zheng Luo <kidraga@gmail.com>
2024-04-22 16:09:40 -0700

    Update indent blankline

--- lua/plugin-config/indent-blankline-config.lua
+++ lua/plugin-config/indent-blankline-config.lua
@@ -1,17 +1,15 @@
 vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
+local highlight = {
+  "Whitespace",
+}
 
 require("ibl").setup {
-  indent = { char = "|" },
-  whitespace = { highlight = { "Whitespace", "NonText" } },
-  scope = { exclude = { language = { "markdown" } } }
-  --  space_char_blankline = " ",
-  --  show_current_context = true,
-  --  show_current_context_start = true,
-  --   show_current_context_end = true,
-  --  show_trailing_blankline_indent = false,
-  --  use_treesitter = true,
-  --  use_treesitter_scope = true,
-  --  filetype_exclude = { "NvimTree", "packer", "help", "CHADTree", "lsp-installer" } -- do not enable in these files. :set ft? to see current filetype
+  indent = {
+    char = "▏",
+    highlight = highlight,
+  },
+  scope = {
+    exclude = { language = { "markdown", "help", "man" } },
+    highlight = highlight
+  },
 }
